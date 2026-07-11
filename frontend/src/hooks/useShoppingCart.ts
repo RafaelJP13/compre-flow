97f0763b446a72a944c00a457eccc00f1feacae6
Rafael Fernandes <rafaelsf013@gmail.com>
2026-07-11 12:04:02 -0300

    chore: remove CartContext and useShoppingCart hook

--- frontend/src/hooks/useShoppingCart.ts
+++
@@ -1,9 +0,0 @@
-import { useCart } from "../context/CartContext";
-
-export const useShoppingCart = () => {
-    const { items, addItem, removeItem, clearCart } = useCart();
-
-    const total = items.reduce((sum, i) => sum + i.price * i.quantity, 0);
-
-    return { cartItems: items, addItem, removeItem, clearCart, total };
-};
